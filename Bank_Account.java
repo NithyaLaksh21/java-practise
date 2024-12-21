42788968b4fe6bc274a6c5127edc8d5d11cc73cd
NithyaLaksh21 <nithyalakshmi.s.21@gmail.com>
2024-12-21 21:30:16 +0530

    Update Bank_Account.java to create a new bankAccount

--- Bank_Account.java
+++ Bank_Account.java
@@ -2,37 +2,6 @@
 
 import java.util.Scanner;
 
-class Bank {
-    String name;
-    private int balance;
-
-    public void deposit() {
-        Scanner userObj = new Scanner(System.in);
-        System.out.println("Enter amount to be deposited" );
-
-        int amount = userObj.nextInt();
-        System.out.println(amount);
-    }
-
-    public void withdraw() {
-        Scanner amt = new Scanner(System.in);
-        System.out.println("Enter amt to be withdrwan");
-
-        int withdrawAmt = amt.nextInt();
-        if ( balance > withdrawAmt )
-        {
-            balance = balance-withdrawAmt;
-            System.out.println("amount withdrawn successfully"+ withdrawAmt);
-        } else {
-            System.out.println("cancelled transaction - insufficient balance");
-        }
-    }
-
-     void getBalance() {
-        System.out.println(balance);
-    }
-    
-}
 public class Bank_Account {
     public static void main(String[] args) {
         Bank bank = new Bank();
@@ -41,6 +10,7 @@ public class Bank_Account {
             System.out.println("Press 1 to deposit");
             System.out.println("Press 2 to widraw amt");
             System.out.println("Press 3 to check balance");
+            System.out.println("Press 4 to create account");
     
             int userChoice = chooseOption.nextInt();
             if ( userChoice == 1 )
@@ -52,6 +22,9 @@ public class Bank_Account {
             }
             else if (userChoice == 3 ){
                 bank.getBalance();
+            }
+            else if (userChoice ==4) {
+                bank.createAccount();
             }
             else {
                 System.out.println("not matching the options");
