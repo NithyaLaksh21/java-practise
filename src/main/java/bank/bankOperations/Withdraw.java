b325ed4a1ca1602759eb7a9bb8a23a2b7cc47b9a
NithyaLaksh21 <nithyalakshmi.s.21@gmail.com>
2024-12-24 14:52:03 +0530

    Update Withdraw.java

--- src/main/java/bank/bankOperations/Withdraw.java
+++ src/main/java/bank/bankOperations/Withdraw.java
@@ -1,19 +1,27 @@
 package src.main.java.bank.bankOperations;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import src.main.java.bank.*;
+import src.main.java.bank.Bank;
 
-public class Withdraw {
+
+public class Withdraw extends AbstractBankOperation {
+
+    @Override
     public void execute(Bank bank) {
         Scanner amt = new Scanner(System.in);
         System.out.println("Enter amt to be withdrawn");
-        int withdrawAmt = amt.nextInt();
-        if (bank.getBalance() > withdrawAmt) {
-            bank.setBalance(bank.getBalance() - withdrawAmt);
-            System.out.println("Amount withdrawn successfully: " + withdrawAmt);
-        } else {
-            System.out.println("Cancelled transaction - insufficient balance");
+        try {
+            int withdrawAmt = amt.nextInt();
+            if (bank.getBalance() > withdrawAmt) {
+                bank.setBalance(bank.getBalance() - withdrawAmt);
+                System.out.println("Amount withdrawn successfully: " + withdrawAmt);
+            } else {
+                System.out.println("Cancelled transaction - insufficient balance");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input, please enter a amount to be withdrawn in number - "+ e);
         }
     }
 }
