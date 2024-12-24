a8fe597b0c695e993bb4a4871c2855ba8d10a373
NithyaLaksh21 <nithyalakshmi.s.21@gmail.com>
2024-12-24 07:14:41 +0530

    Delete bankOperation directory

--- bankOperation/GetBalance.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.bank.bankOperations;
-
-import src.main.java.bank.*;
-
-public class GetBalance {
-    public void execute(Bank bank) {
-        System.out.println("Current balance: " + bank.getBalance());
-    }
-}
