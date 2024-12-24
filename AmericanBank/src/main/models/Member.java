5d9de477f9a8182a00d33dfca2b12283e0dce9d6
NithyaLaksh21 <nithyalakshmi.s.21@gmail.com>
2024-12-24 19:52:48 +0530

    Create Member.java

---
+++ AmericanBank/src/main/models/Member.java
@@ -0,0 +1,42 @@
+package main.models;
+
+public class Member {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String dob;
+    private String phoneNumber;
+    private String emailId;
+
+    public Member(String firstName, String lastName, String address, String dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.dob = dob;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+}
