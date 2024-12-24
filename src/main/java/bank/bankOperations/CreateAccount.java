e58a5eda3989910a2129f7b94816096b5972be72
NithyaLaksh21 <nithyalakshmi.s.21@gmail.com>
2024-12-24 14:51:21 +0530

    Update CreateAccount.java

--- src/main/java/bank/bankOperations/CreateAccount.java
+++ src/main/java/bank/bankOperations/CreateAccount.java
@@ -1,25 +1,32 @@
 package src.main.java.bank.bankOperations;
 
-import src.main.java.bank.utils.*;
 import src.main.java.bank.enums.*;
 import src.main.java.bank.models.*;
+import src.main.java.bank.utils.GetUserAccountType;
+import src.main.java.bank.utils.GetUserDetails;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.UUID;
 
 public class CreateAccount {
     public void execute() {
-        Scanner userInput = new Scanner(System.in);
+        Scanner userInputToChooseAccountType = new Scanner(System.in);
         System.out.println("Enter the Account type you wish to create");
         System.out.println("Press 1 for Savings account");
         System.out.println("Press 2 for Current account");
 
-        int userChoice = userInput.nextInt();
-        AccountType accountType = new GetUserAccountType().execute(userChoice);
-        Member member = new GetUserDetails().execute();
 
-        UUID accNumber = UUID.randomUUID();
-        System.out.println(accountType + " Account created successfully for below details");
-        System.out.println("Account number: " + accNumber + " for " + member.getFirstName() + " " + member.getLastName() + " living in " + "Address: " + member.getAddress() + " DOB: " + member.getDob());
+        try {
+            int userChoice = userInputToChooseAccountType.nextInt();
+            AccountType accountType = new GetUserAccountType().execute(userChoice);
+            Member member = new GetUserDetails().execute();
+
+            UUID accNumber = UUID.randomUUID();
+            System.out.println(accountType + " Account created successfully for below details");
+            System.out.println("Account number: " + accNumber + " for " + member.getFirstName() + " " + member.getLastName() + " living in " + "Address: " + member.getAddress() + " DOB: " + member.getDob());
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input, please enter a number - "+ e);
+        }
     }
 }
