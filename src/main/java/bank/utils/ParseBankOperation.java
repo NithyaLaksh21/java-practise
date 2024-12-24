32eadd7b83a606a4a4bd2fee3ec32b1a6e5dfedd
NithyaLaksh21 <nithyalakshmi.s.21@gmail.com>
2024-12-24 14:56:28 +0530

    Update ParseBankOperation.java

--- src/main/java/bank/utils/ParseBankOperation.java
+++ src/main/java/bank/utils/ParseBankOperation.java
@@ -1,10 +1,21 @@
 package src.main.java.bank.utils;
 
-import src.main.java.bank.enums.BankOperation;
+import src.main.java.bank.enums.OperationType;
 
 public class ParseBankOperation {
-        public static BankOperation parseUserInput(int userInput) {
-            BankOperation bankOperation[] = BankOperation.values();
-            return bankOperation[userInput - 1];
+        public static OperationType parseUserInput(int userInput) {
+            return switch (userInput) {
+                case 1 -> OperationType.DEPOSIT;
+                case 2 -> OperationType.WITHDRAW;
+                case 3 -> OperationType.GETBALANCE;
+                default -> null;
+            };
         }
     }
+
+// public class ParseBankOperation {
+//         public static BankOperation parseUserInput(int userInput) {
+//             BankOperation bankOperation[] = BankOperation.values();
+//             return bankOperation[userInput - 1];
+//         }
+//     }
